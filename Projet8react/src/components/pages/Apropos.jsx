1f261c16862e599a14f28031ad9691091be7ae87
stephanieEt <stetourneau@laposte.net>
2024-03-22 17:44:30 +0100

    refactoring code

--- Projet8react/src/components/pages/Apropos.jsx
+++ Projet8react/src/components/pages/Apropos.jsx
@@ -1,20 +1,25 @@
 import Accordion from "../Accordion";
 import values from "../../constante/apropos.js";
+import Montagne from "../../assets/images/bannerApropos.png";
 
 const Apropos = () => {
   return (
-    <div className="apropos">
-      <img src="./src/assets/images/bannerApropos.png" alt="Photo Montagne" />
-      {values.map((object) => {
-        return (
-          <Accordion
-            key={object.id}
-            title={object.title}
-            text={object.text}
-          ></Accordion>
-        );
-      })}
-    </div>
+    <>
+      <div className="banner">
+        <img src={Montagne} alt="Photo Montagne" />
+      </div>
+      <div>
+        {values.map((object) => {
+          return (
+            <Accordion
+              key={object.id}
+              title={object.title}
+              text={object.text}
+            ></Accordion>
+          );
+        })}
+      </div>
+    </>
   );
 };
 export default Apropos;
